0e7907e1697456b522ef6a8eea83b80163c03f70
Node <8974108+qwenode@users.noreply.github.com>
2021-08-23 11:45:00 +0800

    新增 下拉框列表的默认选项,全部

---
+++ src/widget/DropdownListOption.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace qwenode\yii2lightning\widget;
+
+class DropdownListOption
+{
+    /**
+     * 下拉框列表的默认选项,全部
+     * @return string[]
+     */
+    public static function prompt()
+    {
+        return [
+            'prompt' => '全部',
+        ];
+    }
+}
